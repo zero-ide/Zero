66403f42d0422df89837e3baec8a53129118a563
ori0o0p <seungwon.kim@ktown4u.com>
2026-01-29 10:30:25 +0900

    test: add failing test for AuthManager URL generation (RED)

---
+++ Tests/ZeroTests/ZeroTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import Zero
+
+final class ZeroTests: XCTestCase {
+    func testAuthURLGeneration() throws {
+        // Given
+        let clientID = "test-client-id"
+        let scope = "repo user"
+        let authManager = AuthManager(clientID: clientID, scope: scope)
+
+        // When
+        let url = authManager.getLoginURL()
+
+        // Then
+        XCTAssertTrue(url.absoluteString.starts(with: "https://github.com/login/oauth/authorize"))
+        XCTAssertTrue(url.absoluteString.contains("client_id=\(clientID)"))
+        XCTAssertTrue(url.absoluteString.contains("scope=\(scope.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)"))
+    }
+}
